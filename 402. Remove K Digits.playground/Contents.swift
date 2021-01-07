34c0fb653416adccf8a874a357a0b05b61f3bb07
Choi You Wan <paulchoi9502@gmail.com>
2021-01-07 18:32:11 +0900

    402. Remove K Digits

---
+++ 402. Remove K Digits.playground/Contents.swift
@@ -0,0 +1,47 @@
+class Solution {
+    func removeKdigits(_ num: String, _ k: Int) -> String {
+        let list = Array(num).map { "\($0)" }
+        var stack = Stack(storage: list)
+        var selectedNumber = stack.count - k
+        var results: String = ""
+
+        guard num.count != k else { return "0" }
+
+        while selectedNumber != 0 {
+            let index = stack.count - selectedNumber
+            
+            let result = stack.compare(to: index)
+            selectedNumber -= 1
+            if results.isEmpty && result == "0" {
+                continue
+            } else {
+                results += result
+            }
+        }
+
+        return results.isEmpty ? "0" : results
+    }
+}
+
+struct Stack {
+    private var storage: [String]
+
+    init(storage: [String]) {
+        self.storage = storage
+    }
+
+    mutating func compare(to index: Int) -> String {
+        let list: [String] = self.storage
+        
+        let min = list[0...index].min()
+        let minIndex = list.firstIndex(of: min!)
+        self.storage = Array(list[minIndex! + 1..<count])
+
+        return min!
+    }
+
+    var count: Int {
+        self.storage.count
+    }
+}
+
