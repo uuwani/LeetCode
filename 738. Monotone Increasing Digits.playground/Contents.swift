94b0f82e2761dff0e2a2a2cb0377ac903d427f96
Choi You Wan <paulchoi9502@gmail.com>
2021-01-09 23:55:28 +0900

    738. Monotone Increasing Digits

---
+++ 738. Monotone Increasing Digits.playground/Contents.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func monotoneIncreasingDigits(_ N: Int) -> Int {
+        var valid = true
+        var resultNumber = N
+
+        while valid {
+            let number = String(resultNumber)
+            var digit: Int = 1
+            var oldValue: Character = number.first!
+            valid = false
+
+            for (index, digitNumber) in number.enumerated() {
+                if index < number.count && oldValue <= digitNumber {
+                    oldValue = digitNumber
+                } else {
+                    for _ in 0..<number.count - index {
+                        digit *= 10
+                    }
+                    valid = true
+                    resultNumber = digit == 1 ? N : (N / digit) * digit - 1
+                    break
+                }
+            }
+        }
+        return resultNumber
+    }
+}
+
+let solution = Solution()
+let result = solution.monotoneIncreasingDigits(4554333333451312314)
+
