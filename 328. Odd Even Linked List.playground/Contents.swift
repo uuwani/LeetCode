401b531ebbc74b44240e41f238ea7cf3ddd73e5a
Choi You Wan <paulchoi9502@gmail.com>
2021-02-23 14:04:20 +0900

    Success

---
+++ 328. Odd Even Linked List.playground/Contents.swift
@@ -0,0 +1,35 @@
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    func oddEvenList(_ head: ListNode?) -> ListNode? {
+
+        let result = odd(head)
+
+        var current = result.lhs
+        while current?.next != nil {
+            current = current?.next
+        }
+        current?.next = result.rhs
+
+        return result.lhs
+    }
+
+    func odd(_ head: ListNode?) -> (lhs: ListNode?, rhs: ListNode?) {
+        guard let slow = head else { return (nil, nil) }
+        guard let fast = head?.next else { return (slow, nil) }
+
+        let next = head?.next?.next
+
+        let result = odd(next)
+        slow.next = result.lhs
+        fast.next = result.rhs
+
+        return (slow, fast)
+    }
+}
